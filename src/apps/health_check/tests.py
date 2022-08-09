636bd8c7c751d030b2b0564b8542b77f5fc57718
@d.kopylov <d.kopylov@reg.ru>
2022-08-09 13:09:07 +0300

    health test fix

--- src/apps/health_check/tests.py
+++ src/apps/health_check/tests.py
@@ -4,5 +4,5 @@ from django.test import TestCase
 class HealthCheckTestCase(TestCase):
 
     def test_health_check(self):
-        response = self.client.get('/healthz/')
+        response = self.client.get('/healthz', follow=True)
         self.assertEqual(response.status_code, 200)
