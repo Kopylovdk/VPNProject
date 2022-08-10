9f8a55af2dac46aba5f17f78ac2c2761c6e1a6ac
@d.kopylov <d.kopylov@reg.ru>
2022-08-10 14:29:06 +0300

    ligic modify

--- src/apps/service/bot/processes.py
+++ src/apps/service/bot/processes.py
@@ -1,13 +1,17 @@
-from apps.service.models import VPNServiceRecord
+from apps.service.models import TelegramUsers
 from telebot.types import User
 
 
 def add_new_tg_user(telegram_user: User) -> None:
-    if not VPNServiceRecord.objects.filter(telegram_id=telegram_user.id):
-        new_record = VPNServiceRecord(
+    if not TelegramUsers.objects.filter(telegram_id=telegram_user.id):
+        new_record = TelegramUsers(
             telegram_id=telegram_user.id,
             telegram_login=telegram_user.username,
             telegram_first_name=telegram_user.first_name,
             telegram_last_name=telegram_user.last_name,
         )
         new_record.save()
+
+
+def add_new_vpn_key_to_tg_user():
+    pass
--- src/apps/service/models.py
+++ src/apps/service/models.py
@@ -1,22 +1,38 @@
 from django.db import models
 
 
-class VPNServiceRecord(models.Model):
+class TelegramUsers(models.Model):
     class Meta:
-        db_table = 'VPNServiceRecord'
-        verbose_name = 'Telegram User VPN Key'
-        verbose_name_plural = 'Telegram User VPN Keys'
+        db_table = 'TelegramUsers'
+        verbose_name = 'Telegram User'
+        verbose_name_plural = 'Telegram Users'
+        constraints = [models.UniqueConstraint(fields=['telegram_id'], name='unique_telegram_id')]
 
-    telegram_id = models.IntegerField(verbose_name='ID телеграм', null=True)
+    telegram_id = models.IntegerField(verbose_name='ID пользователя телеграм', null=True)
     telegram_login = models.CharField(verbose_name='Логин телеграм', max_length=254, null=True, blank=True)
     telegram_first_name = models.CharField(verbose_name='Имя телеграм', max_length=254, null=True, blank=True)
     telegram_last_name = models.CharField(verbose_name='Фамилия телеграм', max_length=254, null=True, blank=True)
 
-    record_created_at = models.DateField(verbose_name='Дата создания записи', auto_now_add=True)
-
-    outline_key_id = models.IntegerField(verbose_name='ID OutLine VPN Key', null=True, blank=True)
-    outline_key_valid_until = models.DateField(verbose_name='Дата окончания подписки', null=True, blank=True)
-    outline_key_active = models.BooleanField(verbose_name='Активность VPN ключа', default=True, null=True, blank=True)
+    created_at = models.DateField(verbose_name='Дата создания записи', auto_now_add=True)
 
     def __str__(self):
-        return f'{self.telegram_login}_{self.telegram_first_name}_{self.telegram_last_name}'
+        return f'{self.telegram_login}_{self.telegram_id}'
+
+
+class OutlineVPNKeys(models.Model):
+    class Meta:
+        db_table = 'OutlineVPNKeys'
+        verbose_name = 'Outline VPN Key'
+        verbose_name_plural = 'Outline VPN Keys'
+
+    telegram_user_record = models.ForeignKey(TelegramUsers, on_delete=models.CASCADE, verbose_name='Владелец ключа')
+    outline_key_id = models.IntegerField(verbose_name='ID OutLine VPN Key', null=True, blank=True)
+    outline_key_name = models.CharField(
+        verbose_name='Имя VPN ключа', max_length=254, null=True, blank=True, default='Отсутствует'
+    )
+    outline_key_valid_until = models.DateField(verbose_name='Дата окончания подписки', null=True, blank=True)
+    outline_key_active = models.BooleanField(verbose_name='Активность VPN ключа', default=True, null=True, blank=True)
+    created_at = models.DateField(verbose_name='Дата создания записи', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.telegram_user_record}_{self.outline_key_name}'
