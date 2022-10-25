7413f6133411b013c5a7223afcd3f220a9342bf2
@d.kopylov <d.kopylov@reg.ru>
2022-10-25 19:44:08 +0400

    get outline object modify

--- src/apps/outline_vpn_admin/outline_api.py
+++ src/apps/outline_vpn_admin/outline_api.py
@@ -1,6 +1,14 @@
-# from outline_vpn.outline_vpn import OutlineVPN
-# from vpnservice.settings import EXTERNAL_CFG
-#
-#
-# def get_outline_client(vpn_server_name: str = 'kz') -> OutlineVPN:
-#     return OutlineVPN(EXTERNAL_CFG['outline_vpn_urls'][vpn_server_name])
+from outline_vpn.outline_vpn import OutlineVPN
+from apps.outline_vpn_admin.models import VPNServer
+from apps.outline_vpn_admin.exceptions import VPNServerDoesNotExist
+
+
+def get_outline_client(vpn_server_name: str = None) -> OutlineVPN:
+    try:
+        if vpn_server_name:
+            client = OutlineVPN(VPNServer.objects.get(name=vpn_server_name))
+        else:
+            client = OutlineVPN(VPNServer.objects.get(is_default=True))
+    except VPNServer.DoesNotExist:
+        raise VPNServerDoesNotExist(message=f'VPN Server {vpn_server_name} does not exist')
+    return client
