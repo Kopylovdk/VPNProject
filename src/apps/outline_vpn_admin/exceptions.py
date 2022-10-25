785df69c081e978e2f37aca374420481c3279548
@d.kopylov <d.kopylov@reg.ru>
2022-10-25 19:42:49 +0400

    exceptions added

--- src/apps/outline_vpn_admin/exceptions.py
+++ src/apps/outline_vpn_admin/exceptions.py
@@ -1,2 +1,21 @@
 class ProcessException(Exception):
     """Базовое исключение"""
+    message = 'BASE_EXCEPTION'
+
+    def __init__(self, message):
+        self.message = message
+
+
+class UserDoesNotExist(ProcessException):
+    """User does not exist"""
+    # message = 'USER_DOES_NOT_EXIST'
+
+
+class TransportDoesNotExist(ProcessException):
+    """Transport does not exist"""
+    # message = 'BOT_DOES_NOT_EXIST'
+
+
+class VPNServerDoesNotExist(ProcessException):
+    """VPNServer does not exist"""
+    # message = 'VPN_SERVER_DOES_NOT_EXIST'
