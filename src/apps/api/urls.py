cec8df4acc81dc48e6963cba263661d2ff8eeca4
@d.kopylov <d.kopylov@reg.ru>
2022-11-03 13:54:33 +0400

    api refactoring

--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
     # get
     path('tariff/', api_views.Tariff.as_view(), name='get_tariffs'),
     path('contact/<str:transport_name>/<int:messenger_id>', api_views.ContactCreateOrUpdate.as_view(), name='get_cont'),
-    path('token/<str:transport_name>/<int:messenger_id>', api_views.VPNToken.as_view(), name='get_client_tokens'),
+    path('token/<str:transport_name>/<int:messenger_id>', api_views.VPNTokens.as_view(), name='get_client_tokens'),
     path('vpn-servers/', api_views.VPNServer.as_view(), name='get_vpn_servers'),
 
     # post
     path('contact/', api_views.ContactCreateOrUpdate.as_view(), name='creat_or_update_contact'),
-    path('token/renew/<int:token_id>', api_views.VPNToken.as_view(), name='renew_exist_token'),
+    path('token/renew/', api_views.VPNTokenRenew.as_view(), name='renew_exist_token'),
     path('token/demo/', api_views.VPNTokenDemo.as_view(), name='create_demo_token'),
-    path('token/new/', api_views.VPNToken.as_view(), name='create_new_token'),
+    path('token/new/', api_views.VPNTokenNew.as_view(), name='create_new_token'),
 
     # patch
     # path('vpn_token/<int:vpn_key_id>/user/<int:tg_id>/<str:srv_name>/', api_views.OutlineVPNKeysModelView.as_view()),
--- src/apps/api/views.py
+++ src/apps/api/views.py
@@ -1,11 +1,16 @@
+from decimal import Decimal
+
 from rest_framework import views
 from rest_framework.permissions import IsAuthenticated
 from apps.api.renderers import BaseJSONRenderer
 from rest_framework import status
 from rest_framework.response import Response
+from apps.outline_vpn_admin import exceptions
 from apps.outline_vpn_admin.processes import get_tariff, create_or_update_contact, token_new, token_renew, \
     get_client_tokens, get_client, token_demo, get_vpn_servers
 import logging
+
+
 log = logging.getLogger(__name__)
 
 
@@ -13,7 +18,7 @@ class BaseAPIView(views.APIView):
     permission_classes = (IsAuthenticated,)
     renderer_classes = (BaseJSONRenderer,)
 
-# TODO: Обработка исключений, тесты
+# TODO: тесты
 
 
 class Tariff(BaseAPIView):
@@ -29,58 +34,105 @@ class VPNServer(BaseAPIView):
 class ContactCreateOrUpdate(BaseAPIView):
     def post(self, request):
         data = request.data
-
-        response = create_or_update_contact(
-            transport_name=data['transport_name'],
-            credentials=data['credentials'],
-        )
-        if 'created' in response["details"]:
-            return Response(response, status=status.HTTP_201_CREATED)
-        return Response(response, status=status.HTTP_200_OK)
-
-    def get(self, request, transport_name, messenger_id):
-        return Response(
-            get_client(
-                transport_name=transport_name,
-                messenger_id=messenger_id
-            ),
-            status=status.HTTP_200_OK
-        )
-
-
-class VPNToken(BaseAPIView):
-    def post(self, request, token_id=None):
-        data = request.data
-        if token_id:
-            response = token_renew(
+        try:
+            response = create_or_update_contact(
                 transport_name=data['transport_name'],
                 credentials=data['credentials'],
-                server_name=data['server_name'],
-                token_id=token_id,
             )
+        except exceptions.TransportDoesNotExist as err:
+            return Response({"details": str(err.message)}, status=status.HTTP_404_NOT_FOUND)
         else:
+            if 'created' in response["details"]:
+                return Response(response, status=status.HTTP_201_CREATED)
+            return Response(response, status=status.HTTP_200_OK)
+
+    def get(self, request, transport_name, messenger_id):
+        try:
+            return Response(
+                get_client(
+                    transport_name=transport_name,
+                    messenger_id=messenger_id
+                ),
+                status=status.HTTP_200_OK
+            )
+        except (exceptions.TransportDoesNotExist, exceptions.UserDoesNotExist) as err:
+            return Response({"details": str(err.message)}, status=status.HTTP_404_NOT_FOUND)
+
+
+class VPNTokenNew(BaseAPIView):
+    def post(self, request):
+        data = request.data
+        try:
             response = token_new(
                 transport_name=data['transport_name'],
                 credentials=data['credentials'],
                 server_name=data['server_name'],
             )
-        return Response(response, status=status.HTTP_201_CREATED)
+        except (
+            exceptions.TransportDoesNotExist,
+            exceptions.UserDoesNotExist,
+            exceptions.VPNServerDoesNotExist
+        ) as err:
+            return Response({"details": str(err.message)}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(response, status=status.HTTP_201_CREATED)
 
+
+class VPNTokenRenew(BaseAPIView):
+    def post(self, request):
+        data = request.data
+        log.info(f"{data=!r}")
+        try:
+            response = token_renew(
+                transport_name=data['transport_name'],
+                credentials=data['credentials'],
+                token_id=data['outline_id'],
+            )
+        except (
+            exceptions.TransportDoesNotExist,
+            exceptions.UserDoesNotExist,
+        ) as err:
+            log.error('DoesNotExist')
+            return Response({"details": str(err.message)}, status=status.HTTP_404_NOT_FOUND)
+        except exceptions.BelongToAnotherUser as err:
+            log.error('BelongToAnotherUser')
+            return Response({"details": str(err.message)}, status=status.HTTP_403_FORBIDDEN)
+        else:
+            return Response(response, status=status.HTTP_201_CREATED)
+
+
+class VPNTokens(BaseAPIView):
     def get(self, request, transport_name, messenger_id):
-        log.debug(f'{transport_name=!r}, {messenger_id=!r}')
-        data = get_client_tokens(
+        try:
+            response = get_client_tokens(
                 transport_name=transport_name,
                 messenger_id=messenger_id,
             )
-        return Response(data, status=status.HTTP_200_OK)
+        except (
+                exceptions.TransportDoesNotExist,
+                exceptions.UserDoesNotExist,
+        ) as err:
+            return Response({"details": str(err.message)}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(response, status=status.HTTP_200_OK)
 
 
 class VPNTokenDemo(BaseAPIView):
     def post(self, request):
         data = request.data
-        response = token_demo(
-            transport_name=data['transport_name'],
-            credentials=data['credentials'],
-            server_name=data['server_name'],
-        )
-        return Response(response, status=status.HTTP_201_CREATED)
+        try:
+            response = token_demo(
+                transport_name=data['transport_name'],
+                credentials=data['credentials'],
+                server_name=data['server_name'],
+            )
+        except exceptions.DemoKeyExist as err:
+            return Response({"details": str(err.message)}, status=status.HTTP_403_FORBIDDEN)
+        except (
+                exceptions.TransportDoesNotExist,
+                exceptions.UserDoesNotExist,
+                exceptions.VPNServerDoesNotExist
+        ) as err:
+            return Response({"details": str(err.message)}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(response, status=status.HTTP_201_CREATED)
