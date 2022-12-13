591f672f74195bebd7f77f1b9c42843059a4e9d1
@d.kopylov <d.kopylov@reg.ru>
2022-12-13 13:06:39 +0400

    cache update modul add

--- src/apps/outline_vpn_admin/cashe_update.py
+++ src/apps/outline_vpn_admin/cashe_update.py
@@ -1,19 +1,18 @@
 import datetime
+from apps.outline_vpn_admin.models import Tariff, Transport, VPNServer
+from django.db.models import Max
+import logging
 
-_cache_update_date = None
-
-
-def get_actual_cache_date() -> datetime.datetime:
-    """Корректно только для случая, когда админка запущена в единичном экземпляре"""
-    global _cache_update_date
-    if _cache_update_date is not None:
-        return _cache_update_date
-    from apps.outline_vpn_admin.models import Tariff, Transport, VPNServer
-    update_cache_date(Tariff.objects.order_by('-updated_at').only('updated_at').first().updated_at)
-    update_cache_date(Transport.objects.order_by('-updated_at').only('updated_at').first().updated_at)
-    update_cache_date(VPNServer.objects.order_by('-updated_at').only('updated_at').first().updated_at)
+log = logging.getLogger(__name__)
 
 
-def update_cache_date(new_date: datetime.datetime):
-    global _cache_update_date
-    _cache_update_date = _cache_update_date and max(_cache_update_date, new_date) or new_date
+def get_actual_cache_date() -> datetime:
+    cache_update_date_tariff = Tariff.objects.aggregate(Max('updated_at'))
+    cache_update_date_transport = Transport.objects.aggregate(Max('updated_at'))
+    cache_update_date_vpnserver = VPNServer.objects.aggregate(Max('updated_at'))
+    cache_update_date_max = max(
+        cache_update_date_tariff["updated_at__max"],
+        cache_update_date_transport["updated_at__max"],
+        cache_update_date_vpnserver["updated_at__max"]
+    )
+    return cache_update_date_max
